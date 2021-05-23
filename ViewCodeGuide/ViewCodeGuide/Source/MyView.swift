d466007d7f455b0500c550df85ef1dd8bdb54777
Emerson Mendes Filho <>
2021-05-23 11:12:54 -0300

    Build title and subtitle label on view's init

--- ViewCodeGuide/ViewCodeGuide/Source/MyView.swift
+++ ViewCodeGuide/ViewCodeGuide/Source/MyView.swift
@@ -12,6 +12,29 @@ final class MyView: UIView {
     init() {
         super.init(frame: .zero)
         self.backgroundColor = .orange
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "My Title"
+        titleLabel.backgroundColor = .gray
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(titleLabel)
+        
+        titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 24).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        
+        let subtitleLabel = UILabel()
+        subtitleLabel.text = "My Subtitle"
+        subtitleLabel.backgroundColor = .gray
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(subtitleLabel)
+        
+        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16).isActive = true
+        subtitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        subtitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        
     }
     
     @available(*, unavailable)
