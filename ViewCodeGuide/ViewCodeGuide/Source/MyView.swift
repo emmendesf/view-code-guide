460a8a4583561308f23969869187c528bdba0d81
Emerson Mendes Filho <>
2021-05-23 11:17:30 -0300

    Separate view creation in methods

--- ViewCodeGuide/ViewCodeGuide/Source/MyView.swift
+++ ViewCodeGuide/ViewCodeGuide/Source/MyView.swift
@@ -13,32 +13,45 @@ final class MyView: UIView {
         super.init(frame: .zero)
         self.backgroundColor = .orange
         
-        let titleLabel = UILabel()
-        titleLabel.text = "My Title"
-        titleLabel.backgroundColor = .gray
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        addSubview(titleLabel)
-        
-        titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 24).isActive = true
-        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
-        titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
-        
-        let subtitleLabel = UILabel()
-        subtitleLabel.text = "My Subtitle"
-        subtitleLabel.backgroundColor = .gray
-        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        addSubview(subtitleLabel)
-        
-        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16).isActive = true
-        subtitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
-        subtitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
-        
+        setupHierarchy()
+        setupConstraints()
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.text = "My Title"
+        titleLabel.backgroundColor = .gray
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return titleLabel
+    }()
+    
+    lazy var subtitleLabel: UILabel = {
+        let subtitleLabel = UILabel()
+        subtitleLabel.text = "My Subtitle"
+        subtitleLabel.backgroundColor = .gray
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return subtitleLabel
+    }()
+    
+    func setupHierarchy() {
+        addSubview(titleLabel)
+        addSubview(subtitleLabel)
+    }
+    
+    func setupConstraints() {
+        titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 24).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+
+        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16).isActive = true
+        subtitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        subtitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
     }
 }
